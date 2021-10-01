6d38d21667f9c1fc74309d122b3ef9de1f940d17
Flyinghead <raphael.jean@gmail.com>
2021-10-01 16:48:01 +0200

    pvr: render timeout when using ggpo or !threaded
    will cause ggpo desyncs with previous versions

--- core/rend/gui.cpp
+++ core/rend/gui.cpp
@@ -429,10 +429,7 @@ void gui_start_game(const std::string& path)
 
 	scanner.stop();
 	gui_state = GuiState::Loading;
-	static std::string path_copy;
-	path_copy = path;	// path may be a local var
-
-	dc_load_game(path.empty() ? NULL : path_copy.c_str());
+	dc_load_game(path);
 }
 
 void gui_stop_game(const std::string& message)
@@ -449,6 +446,8 @@ void gui_stop_game(const std::string& message)
 	}
 	else
 	{
+		if (!message.empty())
+			ERROR_LOG(COMMON, "Flycast has stopped: %s", message.c_str());
 		// Exit emulator
 		dc_exit();
 	}
